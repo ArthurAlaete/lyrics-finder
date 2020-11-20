c773e737ebd812c8704cc3ac846f0dc6a9b382a6
ArthurAlaete <arthuralaetelopes@gmail.com>
2020-11-20 17:24:40 -0300

    funcoes e ajustes no css

--- app.js
+++ app.js
@@ -5,26 +5,63 @@ const searchInput = document.querySelector('#search')
 const songsContainer = document.querySelector('#songs-container')
 const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
+const fetchData = async url => {
+    const response = await fetch(url)
+    return await response.json()
+
+} 
+
+const fetchSongs = async term => {
+    const data = await fetchData(`${api}/suggest/${term}`)
+    insertSongsIntoPage(data)
+
+}
+
+const fetchLyrics = async (artist, songTitle) => {
+    const data = await fetchData(`${api}/v1/${artist}/${songTitle}`)
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+
+    songsContainer.innerHTML = `
+        <li>
+            <h2 class="song-artist"><strong>${artist}</strong> - ${songTitle}</h2>
+            <p class="lyrics">${lyrics}</p>
+        </li>
+    `
+
+}
+
+const getMoreSongs = async url => {
+    const data = await fetchData(`https://cors-anywhere.herokuapp.com/${url}`)
+    insertSongsIntoPage(data)
+}
+
+
 const insertSongsIntoPage = songsInfo => {
-    const lis = songsInfo.data.map(song => 
-    `<li>
-        <h3>${song.title}</h3>
-        <h4>${song.artist.name}</h4>
+    console.log(songsInfo)
+    const lis = songsInfo.data.map(song =>
+    `<li class="song">
+        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra</button>
     </li>
     `)
     .reduce((accumulator, li)  => accumulator += li , '')
 
     songsContainer.innerHTML = lis
+
+    if (songsInfo.prev || songsInfo.next) {
+        prevAndNextContainer.innerHTML = `
+            ${ songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : '' }
+            ${ songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : '' }
+        `
+        return
+    }
+
+    prevAndNextContainer.innerHTML = ''
 }   
 
-const fetchSongs = async term => {
-    const response = await fetch(`${api}/suggest/${term}`)
-    const data = await response.json()
 
-    insertSongsIntoPage(data)
-
-}
-
+// Events Listeners //
 
 form.addEventListener('submit', event => {
     event.preventDefault()
@@ -38,4 +75,18 @@ form.addEventListener('submit', event => {
     
     fetchSongs(searchValue) 
     console.log(searchValue)
+})
+
+
+songsContainer.addEventListener('click', event => {
+    const clickedElement = event.target
+    
+    if(clickedElement.tagName === "BUTTON") {
+        const artist = clickedElement.getAttribute('data-artist')
+        const songTitle = clickedElement.getAttribute('data-song-title')
+
+
+        prevAndNextContainer.innerHTML = ''
+        fetchLyrics(artist, songTitle)
+    }
 })
